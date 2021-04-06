c7a0c5387b607624f0f70c13f2e1ea7f23bcc72c
Brian M. Viana <brianmviana@gmail.com>
2021-04-06 16:59:57 -0300

    adding Enemies

---
+++ Assets/_Scripts/BillBoard.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets._Scripts {
+    public class BillBoard : MonoBehaviour {
+
+        void Update() {
+            transform.LookAt(Camera.main.transform);
+        }
+    }
+}
